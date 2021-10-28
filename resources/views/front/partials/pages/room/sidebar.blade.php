5f565666b6be41835bcd7826b05fd4d8d2bd7c01
AlexisVS <alexis.vansan1440@gmail.com>
2021-10-28 17:52:40 +0200

    commencement des formulaires

--- resources/views/front/partials/pages/room/sidebar.blade.php
+++ resources/views/front/partials/pages/room/sidebar.blade.php
@@ -5,18 +5,21 @@
       <div id="booking-notification" class="notification"></div>
       <h3 class="form-title">{{ $static_room_sidebar->sidebar_title }}</h3>
       <div class="inner">
-        <form id="booking-form">
+        <form id="booking-form" action="/room/show/send-form" method="POST">
+          @csrf
           <!-- EMAIL -->
           <div class="form-group">
-            <input class="form-control" name="booking-email" type="email" placeholder="{{ $static_room_sidebar->sidebar_form_email_placeholder }}">
+            <input class="form-control" name="booking-email" value="{{ auth()->user()->email ?? '' }}" {{ auth()->user()
+            ? 'disabled' : null }} type="email" placeholder="{{ $static_room_sidebar->sidebar_form_email_placeholder
+            }}">
           </div>
           <!-- ROOM TYPE -->
           <div class="form-group">
-            <select class="form-control" name="booking-roomtype" title="{{ $static_room_sidebar->sidebar_form_roomtype_title }}" data-header="{{ $static_room_sidebar->sidebar_form_roomtype_data_header }}"
-              disabled="disabled">
-              <option value="Single" selected="selected">Single Room</option>
-              <option value="Double">Double Room</option>
-              <option value="Deluxe">Deluxe Room</option>
+            <select class="form-control" name="booking-roomtype"
+              title="{{ $static_room_sidebar->sidebar_form_roomtype_title }}"
+              data-header="{{ $static_room_sidebar->sidebar_form_roomtype_data_header }}" disabled="disabled">
+              <option value="{{ $show->categories->value }}" selected="selected">{{ $show->categories->text }}</option>
+
             </select>
           </div>
           <!-- DATE -->
@@ -36,7 +39,8 @@
                 <div class="guests-buttons">
                   <label>{{ $static_room_sidebar->sidebar_form_label1 }}
                     <a href="#" title="" data-toggle="popover" data-placement="top" data-trigger="hover"
-                      data-content="{{ $static_room_sidebar->sidebar_form_label1_data_content }}" data-original-title="{{ $static_room_sidebar->sidebar_form_label1_data_title }}">
+                      data-content="{{ $static_room_sidebar->sidebar_form_label1_data_content }}"
+                      data-original-title="{{ $static_room_sidebar->sidebar_form_label1_data_title }}">
                       <i class="fa {{ $static_room_sidebar->sidebar_form_label1_i_class }}"></i>
                     </a>
                   </label>
@@ -49,7 +53,8 @@
                 <div class="guests-buttons">
                   <label>{{ $static_room_sidebar->sidebar_form_label2 }}
                     <a href="#" title="" data-toggle="popover" data-placement="top" data-trigger="hover"
-                      data-content="{{ $static_room_sidebar->sidebar_form_label2_data_content }}" data-original-title="{{ $static_room_sidebar->sidebar_form_label2_data_title }}">
+                      data-content="{{ $static_room_sidebar->sidebar_form_label2_data_content }}"
+                      data-original-title="{{ $static_room_sidebar->sidebar_form_label2_data_title }}">
                       <i class="fa {{ $static_room_sidebar->sidebar_form_label2_i_class }}"></i>
                     </a>
                   </label>
@@ -60,10 +65,55 @@
                   </div>
                 </div>
               </div>
+
             </div>
           </div>
+          <script>
+            {{-- ! C'est claqué au sol mais ça à la qualité de parvenir a ses fin --}}
+            let divPlus, divMinus;
+            divPlus = [...document.querySelectorAll('.plus')];
+            divPlus.map( function (e) {e.addEventListener('click', function (event) {
+
+              let divPlus, guestTotal;
+              var numberPersons = {!!  json_encode($show->number_persons, JSON_HEX_TAG) !!};
+
+              divPlus = [...document.querySelectorAll('.plus')];
+
+              guestTotal = document.querySelector('.gueststotal');
+              guestTotal = guestTotal.innerText;
+
+              if (guestTotal < numberPersons) {
+                divPlus.forEach( e => e.style.visibility = 'visible');
+              }
+              if (guestTotal == numberPersons - 1) {
+                divPlus.forEach( e => e.style.visibility = 'hidden');
+              }
+
+
+            })});
+
+            divMinus = [...document.querySelectorAll('.minus')];
+            divMinus.map( function (e) {e.addEventListener('click', function (event) {
+
+              let divPlus, divMinus, guestTotal;
+              var numberPersons = {!!  json_encode($show->number_persons, JSON_HEX_TAG) !!};
+
+              divPlus = [...document.querySelectorAll('.plus')];
+              divMinus = [...document.querySelectorAll('.minus')]
+
+              guestTotal = document.querySelector('.gueststotal');
+              guestTotal = guestTotal.innerText;
+
+              if (guestTotal == 1) {
+                divPlus.forEach( e => e.style.visibility = 'visible');
+                divMinus.forEach( e => e.style.visibility = 'visible');
+              }
+            })});
+          </script>
+
           <!-- BOOKING BUTTON -->
-          <button type="submit" class="btn btn-dark btn-fw mt20 mb20">{{ $static_room_sidebar->sidebar_form_submit }}</button>
+          <button type="submit" class="btn btn-dark btn-fw mt20 mb20">{{ $static_room_sidebar->sidebar_form_submit
+            }}</button>
         </form>
       </div>
     </div>
